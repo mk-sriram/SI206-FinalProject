f59ccc8ffa9cea9941eacb78660927fc0b7aa0c5
Sriram MK <mksriram@umich.edu>
2024-12-04 05:08:28 -0500

    added new functions
    - trying to update the weather table by getting the values from the footbal table
    - before : data was combined, ran into concurrency issues with the data

--- sqlfunctions.py
+++ sqlfunctions.py
@@ -144,7 +144,7 @@ def fetch_weather(lat, lon, start_date, end_date):
     Fetch hourly weather data for a location using the Open-Meteo SDK.
     """
     try:
-        print(lat, lon, start_date, end_date)
+        #print(lat, lon, start_date, end_date)
         url = f"https://archive-api.open-meteo.com/v1/archive?latitude={lat}&longitude={lon}&start_date={start_date}&end_date={end_date}&hourly=temperature_2m,relative_humidity_2m,precipitation,cloud_cover,wind_speed_10m,wind_direction_10m"
         response = requests.get(url)
         response.raise_for_status()
@@ -175,8 +175,31 @@ def create_weather_table():
     conn.close()
     print("Table 'weather_data' created successfully with additional fields.")
 
+from datetime import datetime
 
-# def addWeatherDataToTable(combined_data):
+from datetime import datetime, timedelta
+
+def find_closest_time_index(time_list, target_time):
+    """
+    Find the index of the closest time in the time_list to the given target_time in Zulu time.
+
+    Args:
+        time_list (list): List of time strings in ISO 8601 format without timezone information (local time).
+        target_time (str): Target time string in ISO 8601 format with 'Z' (UTC).
+
+    Returns:
+        int: Index of the closest time in the list.
+    """
+    target_dt_utc = datetime.strptime(target_time, "%H:%M:%S.%fZ")
+
+    # Parse the time list into naive datetime objects
+    parsed_times = [datetime.strptime(t, "%Y-%m-%dT%H:%M") for t in time_list]
+
+    # Find the index of the closest time
+    closest_index = min(range(len(parsed_times)), key=lambda i: abs(parsed_times[i] - target_dt_utc)) 
+    return closest_index # Return the index and the closest time
+
+
 #     """
 #     Fetch weather data for games and add it to the weather_data table, linking it with football_games.
 #     """
@@ -235,83 +258,145 @@ def create_weather_table():
 #     conn.commit()
 #     conn.close()
 #     print("Weather data successfully added to the 'weather_data' table and linked to 'football_games'.")
-def addWeatherDataToTable(combined_data):
+# def addWeatherDataToTable(combined_data):
+#     """
+#     Fetch weather data for games and add it to the weather_data table, linking it with football_games.
+#     """
+#     conn = sqlite3.connect("football_data.db")
+#     cursor = conn.cursor()
+
+#     for game in combined_data:
+#         lat = game["latitude"]
+#         lon = game["longitude"]
+#         date = game["game_date"]
+#         id = game["game_id"]
+#         gameTime = game["game_time"]
+#         if lat and lon and date:
+#             # Fetch weather data for the game's location and date
+#             weather_data = fetch_weather(lat, lon, date, date)
+#             if weather_data:
+#                 print(weather_data["hourly"]["time"]) 
+#                 print(gameTime)
+#                 print(id) 
+                
+#                 correct_index = find_closest_time_index(weather_data["hourly"]["time"],gameTime)
+#                 print("index :",correct_index)
+#                 # temperature
+#                 # precipitation
+#                 # snowfall
+#                 # wind_speed
+#                 # wind_direction
+#                 # humidity
+#                 # cloud_cover
+#                 # cursor.execute('''
+#                 #         INSERT INTO weather_data (``
+#                 #             city, latitude, longitude, weather_date, temperature, 
+#                 #             precipitation, snowfall, wind_speed, wind_direction, humidity, 
+#                 #             cloud_cover
+#                 #         ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+#                 #         ''', (
+#                 #             game["city"],
+#                 #             lat,
+#                 #             lon,
+#                 #             date,
+#                 #             temperature,
+#                 #             precipitation,
+#                 #             snowfall,
+#                 #             wind_speed,
+#                 #             wind_direction,
+#                 #             humidity,
+#                 #             cloud_cover
+#                 #         ))
+
+#                 #         # Get the last inserted weather_id
+#                 #         weather_id = cursor.lastrowid
+
+#                 #         # Update football_games table with the weather_id
+#                 #         cursor.execute('''
+#                 #         UPDATE football_games SET weather_id = ? WHERE game_id = ?
+#                 #         ''', (weather_id, game["game_id"]))
+#     conn.commit()
+#     conn.close()
+#     print("Weather data successfully added to the 'weather_data' table and linked to 'football_games'.")
+
+def fetch_football_data_from_db():
     """
-    Fetch weather data for games and add it to the weather_data table, linking it with football_games.
+    Fetch all rows from the football_games table.
     """
     conn = sqlite3.connect("football_data.db")
     cursor = conn.cursor()
+    cursor.execute("SELECT game_id, game_date, game_time, city, latitude, longitude FROM football_games WHERE weather_id IS NULL")
+    rows = cursor.fetchall()
+    conn.close()
+    return rows
 
-    for game in combined_data:
-        lat = game["latitude"]
-        lon = game["longitude"]
-        date = game["game_date"]
+def addWeatherDataFromDb():
+    """
+    Fetch weather data for football games and update the database.
+    """
+    # Fetch data from football_games table
+    football_data = fetch_football_data_from_db()
+    
+    if not football_data:
+        print("No football games to process for weather data.")
+        return
 
-        if lat and lon and date:
-            # Fetch weather data for the game's location and date
-            weather_data = fetch_weather(lat, lon, date, date)
-            if weather_data:
-                # Process the hourly data
-                print(game["game_id"], " ", weather_data)
-                # hourly = weather_data.Hourly()
-                # if hourly:
-                    #print()
-                    # Extract time and variables
-                    # times = [hourly.Time(i) for i in range(hourly.TimeLength())]
-                    # temperature_values = [hourly.Variables(Variable.temperature_2m).Values(i) for i in range(hourly.VariablesLength())]
-                    # precipitation_values = [hourly.Variables(Variable.precipitation).Values(i) for i in range(hourly.VariablesLength())]
-                    # snowfall_values = [hourly.Variables(Variable.snowfall).Values(i) for i in range(hourly.VariablesLength())]
-                    # wind_speed_values = [hourly.Variables(Variable.wind_speed_10m).Values(i) for i in range(hourly.VariablesLength())]
-                    # wind_direction_values = [hourly.Variables(Variable.wind_direction_10m).Values(i) for i in range(hourly.VariablesLength())]
-                    # humidity_values = [hourly.Variables(Variable.relative_humidity_2m).Values(i) for i in range(hourly.VariablesLength())]
-                    # cloud_cover_values = [hourly.Variables(Variable.cloud_cover).Values(i) for i in range(hourly.VariablesLength())]
+    conn = sqlite3.connect("football_data.db")
+    cursor = conn.cursor()
 
-                    # # Match the closest time to the game's start time (assumes "game_time" is HH:MM)
-                    # game_time = f"{date}T{game['game_time']}" if game["game_time"] else None
-                    # if game_time in times:
-                    #     index = times.index(game_time)
+    for game in football_data:
+        game_id, game_date, game_time, city, lat, lon = game
 
-                    #     # Retrieve weather details for the matching time
-                    #     temperature = temperature_values[index]
-                    #     precipitation = precipitation_values[index]
-                    #     snowfall = snowfall_values[index]
-                    #     wind_speed = wind_speed_values[index]
-                    #     wind_direction = wind_direction_values[index]
-                    #     humidity = humidity_values[index]
-                    #     cloud_cover = cloud_cover_values[index]
+        # Skip rows with missing location data
+        if not lat or not lon or not game_date:
+            print(f"Skipping game {game_id} due to missing location or date.")
+            continue
 
-                    #     # Insert weather data into the weather_data table
-                    #     cursor.execute('''
-                    #     INSERT INTO weather_data (
-                    #         city, latitude, longitude, weather_date, temperature, 
-                    #         precipitation, snowfall, wind_speed, wind_direction, humidity, 
-                    #         cloud_cover
-                    #     ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-                    #     ''', (
-                    #         game["city"],
-                    #         lat,
-                    #         lon,
-                    #         date,
-                    #         temperature,
-                    #         precipitation,
-                    #         snowfall,
-                    #         wind_speed,
-                    #         wind_direction,
-                    #         humidity,
-                    #         cloud_cover
-                    #     ))
+        # Fetch weather data for the game's location and date
+        weather_data = fetch_weather(lat, lon, game_date, game_date)
+        if not weather_data or "hourly" not in weather_data:
+            print(f"Failed to fetch weather data for game {game_id}.")
+            continue
 
-                    #     # Get the last inserted weather_id
-                    #     weather_id = cursor.lastrowid
+        # Find the closest time index
+        hourly_times = weather_data["hourly"]["time"]
+        closest_index = find_closest_time_index(hourly_times, game_time)
 
-                    #     # Update football_games table with the weather_id
-                    #     cursor.execute('''
-                    #     UPDATE football_games SET weather_id = ? WHERE game_id = ?
-                    #     ''', (weather_id, game["game_id"]))
+        # Extract relevant weather data
+        hourly = weather_data["hourly"]
+        temperature = hourly["temperature_2m"][closest_index]
+        precipitation = hourly["precipitation"][closest_index]
+        wind_speed = hourly["wind_speed_10m"][closest_index]
+        wind_direction = hourly["wind_direction_10m"][closest_index]
+        humidity = hourly["relative_humidity_2m"][closest_index]
+        cloud_cover = hourly["cloud_cover"][closest_index]
 
+        # Insert weather data into weather_data table
+        cursor.execute('''
+            INSERT INTO weather_data (
+                city, latitude, longitude, weather_date, temperature, 
+                precipitation, wind_speed, wind_direction, humidity, 
+                cloud_cover
+            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        ''', (
+            city, lat, lon, game_date, temperature, 
+            precipitation, wind_speed, wind_direction, 
+            humidity, cloud_cover
+        ))
+
+        # Get the last inserted weather_id
+        weather_id = cursor.lastrowid
+
+        # Update football_games table with the weather_id
+        cursor.execute('''
+            UPDATE football_games SET weather_id = ? WHERE game_id = ?
+        ''', (weather_id, game_id))
+
+    # Commit changes and close connection
     conn.commit()
     conn.close()
     print("Weather data successfully added to the 'weather_data' table and linked to 'football_games'.")
+    
 
 #Main stuff
 year = 2020
@@ -338,4 +423,4 @@ create_weather_table()
 
 # Add weather data and link to football_games table
 print("Fetching weather data and updating tables...")
-addWeatherDataToTable(combined_data)
+addWeatherDataFromDb()
