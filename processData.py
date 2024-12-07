5f4fe903bff707e05e41d98c36ceafb64c22903f
emilyj <emilyj@MacBook-Pro-1106.local>
2024-12-06 20:37:29 -0500

    fix_charli_xcx_name

---
+++ processData.py
@@ -0,0 +1,13 @@
+#importing libraries
+import sqlite3
+
+
+#JOIN databases
+
+
+#SELECT football_games.total_points, weather_data.temperature, weather_data.precipitation, weather_data.wind_speed
+#FROM football_games
+#INNER JOIN weather_data ON football_games.weather_id=weather_data.weather_id
+
+
+#saving data (total_points, temperature, precipitation, wind_speed) to CSV file
